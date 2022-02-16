6653b6fa0aa2ef758a36a9abbc54faf152c9d2da
Ner <Nerijuscep@gmail.com>
2022-02-15 20:28:56 -0600

    Bean Annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/FullTimeMentor.java
@@ -0,0 +1,10 @@
+package com.cydeo.bean_annotation;
+
+public class FullTimeMentor {
+
+
+        public void createAccount() {
+            System.out.println("Full time Mentor is created");
+        }
+    }
+
