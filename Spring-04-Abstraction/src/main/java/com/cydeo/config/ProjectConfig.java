39b67fa6e44ad17cb3643234a42d3d158bd32caa
Ner <Nerijuscep@gmail.com>
2022-02-22 19:42:37 -0600

    @Qualifier

---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/config/ProjectConfig.java
@@ -0,0 +1,12 @@
+package com.cydeo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@ComponentScan (basePackages = "com.cydeo")
+// or add all in one place
+@ComponentScan (basePackages = {"com.cydeo.proxy", "com.cydeo.service", "com.cydeo.repository"})
+
+public class ProjectConfig {
+}
