1e58905ce0222b35834156df4dca34fb356ec1de
Ner <Nerijuscep@gmail.com>
2022-03-01 20:14:21 -0600

    Spring MVC Controller

---
+++ Spring-07-Spring-MVC-Contoller/src/test/java/com/cydeo/spring07springmvccontoller/Spring07SpringMvcContollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring07springmvccontoller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07SpringMvcContollerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
