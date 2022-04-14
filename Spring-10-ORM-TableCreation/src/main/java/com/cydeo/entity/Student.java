207532a2bfeb9b4bab8fdd62b80b010df0b4846c
Ner <Nerijuscep@gmail.com>
2022-04-14 18:43:33 -0500

    table annotations

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,13 +1,35 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.cydeo.enums.Gender;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
+@Table(name = "students")
+
 public class Student {
 @Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+@Column(name = "studentFirstName")
     private String firstName;
+@Column(name = "studentLastName")
     private String lastName;
     private String email;
+
+    //this field will not be saved to the table
+    @Transient
+    private String city;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate birthDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 }
--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,5 @@
-INSERT INTO EMPLOYEES(id,name)
-VALUES(1,'Mike Smith');
+-- INSERT INTO EMPLOYEES(id,name)
+-- VALUES(1,'Mike Smith');
+
+INSERT INTO Student(first_name, last_name, email)
+VALUES('Mike', 'Smith', 'mike@test.com');
